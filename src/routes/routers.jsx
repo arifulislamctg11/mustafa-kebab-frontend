c500f138d521b42bfcfb8b3e4c411947cbfd6e47
Md Yeasin <mdyeasinhn@gmail.com>
2024-10-31 00:20:13 +0600

    authontaction setup

--- src/routes/routers.jsx
+++ src/routes/routers.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layout/MainLayout";
 import Home from "../pages/Home/Home";
+import SignIn from "../pages/Authontaction/SignIn";
+import SignUp from "../pages/Authontaction/SignUp";
 
 export const router = createBrowserRouter([
     {
@@ -12,6 +14,14 @@ export const router = createBrowserRouter([
           path: '/',
           element: <Home />,
         },
+        {
+          path : '/signin',
+          element: <SignIn/>
+        },
+        {
+          path : '/signup',
+          element: <SignUp/>
+        }
      
       ],
     },
