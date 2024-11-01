a2162c1d480f7b8c9764f4f033896b24a671fbbf
Md Yeasin <mdyeasinhn@gmail.com>
2024-11-01 16:02:09 +0600

    signin and signup page desgin complete

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,9 +1,15 @@
 import { useContext } from 'react'
-import { AuthContext } from '../../src/Provider/AuthProvider.jsx'
+import { AuthContext } from '../Provider/AuthProvider'
+
 
 const useAuth = () => {
   const auth = useContext(AuthContext)
   return auth
 }
 
-export default useAuth
+export default useAuth 
+
+
+
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import { router } from './routes/routers'
+import AuthProvider from './Provider/AuthProvider'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+     <AuthProvider>
+     <RouterProvider router={router} />
+     </AuthProvider>
   </QueryClientProvider>
 )
