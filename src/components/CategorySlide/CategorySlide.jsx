4d7618cddbeb658f1e769f695969ca46066dcd44
md rasif <mdrasif3499@gmail.com>
2024-11-10 13:44:06 +0600

    serach section create

--- src/components/CategorySlide/CategorySlide.jsx
+++ src/components/CategorySlide/CategorySlide.jsx
@@ -1,4 +1,3 @@
-// CategorySlide.js
 import React, { useRef } from 'react';
 import { RiMenuFold2Fill } from 'react-icons/ri';
 import { TiChevronLeft, TiChevronRight } from 'react-icons/ti';
@@ -15,7 +14,7 @@ const CategorySlide = () => {
     'Kapsalon',
     'Chicken nuggets',
     'Chicken nuggets',
-    'Chicken nuggets'
+    'Chicken nuggets',
   ];
 
   // Function to handle scrolling to the left
@@ -23,7 +22,7 @@ const CategorySlide = () => {
     if (scrollRef.current) {
       scrollRef.current.scrollBy({
         left: -150,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
     }
   };
@@ -33,47 +32,47 @@ const CategorySlide = () => {
     if (scrollRef.current) {
       scrollRef.current.scrollBy({
         left: 150,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
     }
   };
 
   return (
-    <div className="flex items-center space-x-4  p-4 bg-white rounded-lg ">
-      {/* Left Arrow Icon */}
+    <div className="flex items-center justify-between space-x-4 p-4  lg:px-0 md:px-0 px-0  bg-white  w-full">
+      {/* Left Arrow Icon - Hidden on small devices */}
       <button
         onClick={scrollLeft}
-        className="p-2 text-black rounded-full hover:bg-gray-300 bg-gray-200"
+        className="hidden md:block p-2 text-black rounded-full hover:bg-gray-300 bg-gray-200"
       >
-        <TiChevronLeft className="h-5 w-5" />
+        <TiChevronLeft className="h-6 w-6 " />
       </button>
 
       {/* Menu Items */}
       <div
         ref={scrollRef}
-        className="flex space-x-6 overflow-x-auto whitespace-nowrap custom-scrollbar-hide"
+        className="flex-1 flex space-x-3 lg:font-normal  md:font-normal font-semibold overflow-x-auto whitespace-nowrap custom-scrollbar-hide scroll-smooth"
       >
         {menuItems.map((item, index) => (
           <button
             key={index}
-            className="px-4 py-2 text-gray-800 rounded-md hover:bg-gray-100 transition"
+            className="px-4 py-2 text-gray-800 text-base lg:text-md xl:text-md    rounded-md hover:bg-gray-100 transition"
           >
             {item}
           </button>
         ))}
       </div>
 
-      {/* Right Arrow Icon */}
+      {/* Right Arrow Icon - Hidden on small devices */}
       <button
         onClick={scrollRight}
-        className="p-2 text-black rounded-full hover:bg-gray-300 bg-gray-200"
+        className="hidden md:block p-2 text-black rounded-full hover:bg-gray-300 bg-gray-200"
       >
-        <TiChevronRight className="h-5 w-5" />
+        <TiChevronRight className="h-6 w-6 " />
       </button>
 
       {/* Menu Icon */}
       <button className="p-2 bg-gray-200 text-gray-800 rounded-full hover:bg-gray-300">
-        <RiMenuFold2Fill className="h-5 w-5" />
+        <RiMenuFold2Fill className="lg:h-6  lg:w-6 md:h-6  md:w-6 h-4 w-4 " />
       </button>
     </div>
   );
